d67918981072b08160c7381613d059ecf4261944
javiercresp <jacrespog@alumnos.unex.es>
2025-11-07 18:32:40 +0100

    fix :cambio total de python a java

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 
 import connexion
+from swagger_ui_bundle import swagger_ui_path
 
 from swagger_server import encoder
 
 
 def main():
-    app = connexion.App(__name__, specification_dir="./swagger/", options={"swagger_ui": True})
+<<<<<<< ours
+    app = connexion.App(__name__, specification_dir='./swagger/', swagger_ui=True)
+=======
+    app = connexion.App(
+        __name__,
+        specification_dir="./swagger/",
+        options={
+            "swagger_ui": True,
+            "swagger_path": str(swagger_ui_path),
+        },
+    )
+>>>>>>> theirs
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'UnderSound Content API'}, pythonic_params=True)
+    app.add_api(
+        'swagger.yaml',
+        arguments={'title': 'UnderSound Content API'},
+        pythonic_params=True,
+        base_path="/v1",
+    )
     app.run(port=8080)
 
 
